33a361518a9d609897413ca2c4eaf8f4cbceb7bc
samuel <samukadesouza1995@gmail.com>
2023-08-19 01:18:52 -0300

    Add project files.

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using app.Models;
+
+namespace app.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, UserName = "batman", Password = "123", Role = "manager" });
+            users.Add(new User { Id = 1, UserName = "robin", Password = "123", Role = "employee" });
+            return users.Where(x => x.UserName.ToLower() == username && x.Password == password).First();
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace app
+{
+    public static class Settings
+    {
+        public static string Secret = "fcd978ca981609f670695eec87dfa70b76cfcf04ff55723f2deaffed7b190ccc";
+    }
+}
